9fae0c530ba9489cb28fa40d6e5c6b8203a9a2c0
Alejandro Fortner <alejandroc0912@gmail.com>
2020-12-07 15:47:35 -0500

    added files to new repo

---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/afortner09",
+    "name": "Alejandro Fortner",
+    "role": "Frontend Developer",
+    "linkedinId":"alejandro-fortner",
+    "roleDescription": "Hello, my name is Alejandro Fortner Cabrera. I am a starter web developer with basic HTML, CSS and JavaScript knowledge.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/alejandro-fortner-4815161a8/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/afortner09",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a student at University of Miami Bootcamp preparing to be a fullstack software developer. I mainly excel in the whole frontend of the application since I'm really into the visual and design of objects and websites.",
+    "address":"Miami",
+    "website":"https://github.com/afortner09",
+    "education":[
+      {
+        "UniversityName":"University of Miami Coding Bootcamp",
+        "specialization":"Fullstack Software Developer",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2020",
+        "Achievements":"Excels in Frontend development"
+      },
+      {
+        "UniversityName":"Instituto De Educación Activa (IDEA); Valencia, Venezuela",
+        "specialization":"High School",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2018",
+        "Achievements":"High School Graduate"
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Polo Ralph Lauren Factory Store",
+        "specialization":"Sales Associate & Mens’s Merchandiser",
+        "MonthOfLeaving":"Apr",
+        "YearOfLeaving":"2020",
+        "Achievements":"My tasks consisted on being able to give excellent customer service and suggest items based on the customer needs to sell the product. I was also entitled to the men’s full timer position after 4 months which allowed me to learn how to merchandise the newly arrived product becoming the department’s main associate and right hand of the department’s manager. I also had to replenish the sales floor by taking out every needed size and was in charge of changing the department’s promotion and markdowns. I have helped on every overnight floor-set since I started working with the company and know how to run every other position including; cashier, fitting room, mannequin wardrobe change, and back stock."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"Stock Invest-Gator",
+        "imgurl":"images/SIG cap copy.png"
+      },
+      {
+        "name":"project2",
+        "description":"Subtrack",
+        "imgurl":"images/Subtrack copy.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"Success is walking from failure to failure with no loss of enthusiasm.",
+        "name":"Winston Churchill"
+      },
+      {
+        "description":"Success isn’t always about ‘greatness’, it’s about consistency. Consistent, hard work gains success.",
+        "name":"Dwayne Johnson"
+      }
+    ]
+  }
+  
+  export default resumeData
