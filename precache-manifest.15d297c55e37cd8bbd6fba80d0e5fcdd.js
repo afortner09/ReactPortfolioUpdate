e2e858caf9d6ee43d74540bcbbc80691fc07e8b4
Alejandro Fortner <alejandroc0912@gmail.com>
2020-12-12 14:26:21 -0500

    Updates

---
+++ precache-manifest.15d297c55e37cd8bbd6fba80d0e5fcdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72e603b1715d831e6028",
+    "url": "/ReactPortfolioUpdate/static/css/main.c219274f.chunk.css"
+  },
+  {
+    "revision": "72e603b1715d831e6028",
+    "url": "/ReactPortfolioUpdate/static/js/main.72e603b1.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/ReactPortfolioUpdate/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "29709d77a00112527ceb",
+    "url": "/ReactPortfolioUpdate/static/js/runtime~main.29709d77.js"
+  },
+  {
+    "revision": "bd04003501f577898683062ef14f02e0",
+    "url": "/ReactPortfolioUpdate/index.html"
+  }
+];
